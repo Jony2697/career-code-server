4dc216e6ea20a167e8f40fbe398066473b6cb5c1
Jony2697 <ijony2697@gmail.com>
2025-07-21 01:53:38 +0600

    status,application count added

--- index.js
+++ index.js
@@ -33,8 +33,44 @@ async function run() {
 
     //jobs api
     app.get('/jobs',async(req,res)=>{
-      const result=await jobsCollection.find().toArray();
+      const email=req.query.email;
+      const query={};
+      if(email){
+          query.hr_email = email
+      }
+      const result=await jobsCollection.find(query).toArray();
       res.send(result);
+    })
+
+    // could be done but should not be done.
+    // app.get('/jobs',async(req,res)=>{
+    //   const email=req.body.email;
+    //   const query={hr_email:email};
+    //   const result=await jobsCollection.find(query).toArray();
+    //   res.send(result);
+
+    // })
+
+    //could be done but should not be done
+    // app.get('/jobsByEmailAddress',async(req,res)=>{
+    //   const email=req.query.email;
+    //   const query={hr_email:email};
+    //   const result= await jobsCollection.find().toArray();
+    //   res.send(result)
+    // })
+
+    app.get('/jobs/applications',async(req,res)=>{
+      const email=req.query.email;
+      const query={hr_email:email};
+      const jobs = await jobsCollection.find(query).toArray();
+      
+      //should  use aggregate to have optimum data fetching
+      for (const job of jobs){
+        const applicationQuery={jobId:job._id.toString()};
+        const application_count=await applicationsCollection.countDocuments(applicationQuery)
+        job.application_count=application_count;
+      }
+      res.send(jobs)
     })
 
     app.get('/jobs/:id',async(req,res)=>{
@@ -44,13 +80,75 @@ async function run() {
       res.send(result);
     })
 
+    app.post('/jobs',async(req,res)=>{
+      const newJob=req.body;
+      console.log(newJob);
+      const result=await jobsCollection.insertOne(newJob);
+      res.send(result);       
+    })
+
     //job applications related api
+
+    app.get('/applications',async(req,res)=>{
+      const email=req.query.email;
+      const query={
+        applicant:email
+      }
+      const result=await applicationsCollection.find(query).toArray();
+
+      //bad way to aggregate data
+      for(const application of result){
+        const jobId=application.jobId;
+        const jobQuery={_id:new ObjectId(jobId)}
+        const job=await jobsCollection.findOne(jobQuery);
+        application.company=job.company
+        application.title=job.title
+        application.company_logo = job.company_logo
+        application.location = job.location
+        application.jobType = job.jobType
+      }
+
+      res.send(result);
+    })
+
+    app.get('/applications/job/:job_id',async(req,res)=>{
+      const job_id=req.params.job_id;
+      console.log(job_id);
+      
+      const query={jobId:job_id};
+      const result=await applicationsCollection.find(query).toArray();
+      res.send(result);
+    })
+
     app.post('/applications',async(req,res)=>{
       const application = req.body;
       console.log(application);     
       const result=await applicationsCollection.insertOne(application);
       res.send(result);
     })
+
+    app.patch('/applications/:id',async(req,res)=>{
+      const id=req.params.id;
+      const filter={_id:new ObjectId(id)}
+      const updatedDoc ={
+        $set:{
+          status:req.body.status
+        }
+      }
+      const result=await applicationsCollection.updateOne(filter,updatedDoc);
+      res.send(result);
+
+
+    })
+
+
+
+    // app.delete('/applications/:id',async(req,res)=>{
+    //   const id=req.params.id;
+    //   const query={_id:new ObjectId(id)};
+    //   const result=await applicationsCollection.deleteOne(query);
+    //   res.send(result);
+    // })
 
 
     // Send a ping to confirm a successful connection
